d2f12afcdb6e0be4b2c135ff213fc7dd9ced8c00
Pedro Paulo <Pedrinho_fsf@hotmail.com>
2021-12-04 03:12:23 -0300

    :construction: Desenvolve o lightbox da interface

--- src/script/presentationProduct.js
+++ src/script/presentationProduct.js
@@ -126,4 +126,155 @@ imagesProducts.forEach((item, index) => {
     if(selected) {
         loadImageCurrentProduct(index);
     }
-})
+});
+
+// LightBox
+
+class LightBoxProduct {
+    constructor({
+        thumbnails
+    } = {}) {
+        function createThumbnailElements({
+            thumbnails = [],
+            element
+        }) {
+            const arrayFull = [];
+
+            const elementsThumbnailArray = thumbnails.map((item, index) => {
+                const { 
+                    thumbnail
+                } = item;
+
+                element.attr("src", thumbnail.source);
+                element.attr("alt", thumbnail.alt);
+                element.attr("data-id", index);
+
+                return [ ...element ];
+            });
+
+            elementsThumbnailArray.forEach(item => {
+                arrayFull.push(...item);
+            })
+
+            return arrayFull;
+        }
+
+        const stylesElements = {
+            area: {
+                "display": "flex",
+                "flex-direction": "column",
+                "justify-content": "center",
+                "align-items": "center"
+            },
+            currentElementImage: {
+                "width": "300"
+            },
+            areaThumbnails: {
+                "display": "flex",
+                "justify-content": "space-around",
+                "align-items": "center"
+            },
+            backgroundDark: {
+                "position": "absolute",
+                "top": 0,
+                "left": 0,
+                "background-color": "rgba(29, 32, 37, 0.5)",
+                "width": "100%",
+                "height": "103vh",
+                "display": "flex",
+                "justify-content": "center",
+                "align-items": "center"
+            }
+        }
+
+        const backgroundDark = $("<div></div>");
+        const area = $("<div></div>");
+        const currentElementImage = $("<img/>");
+        const areaThumbnails = $("<div/>");
+        const thumbnailElement = $("<img/>");
+        
+        backgroundDark.css(stylesElements.backgroundDark);
+        areaThumbnails.css(stylesElements.areaThumbnails);
+        area.css(stylesElements.area);
+        currentElementImage.css(stylesElements.currentElementImage);
+
+        const { normal } = thumbnails[0];
+
+        currentElementImage.attr("src", normal.source);
+        currentElementImage.attr("alt", normal.alt);
+
+        this._backgroundDark = backgroundDark;
+        this._area = area;
+        this._currentElementImage = currentElementImage;
+        this._areaThumbnails = areaThumbnails;
+        this._thumbnailsElements = createThumbnailElements({
+            thumbnails,
+            element: thumbnailElement
+        });
+    }
+
+    get backgroundDark() {
+        return this._backgroundDark;
+    }
+
+    get area() {
+        return this._area;
+    };
+
+    get currentElementImage() {
+        return this._currentElementImage;
+    };
+
+    get areaThumbnails() {
+        return this._areaThumbnails;
+    }
+    
+    get thumbnailsElements() {
+        return this._thumbnailsElements;
+    }
+
+    show() {
+        const {
+            backgroundDark,
+            area,
+            currentElementImage,
+            areaThumbnails,
+            thumbnailsElements
+        } = this;
+
+        function mountLightBox({
+            backgroundDark,
+            area,
+            currentElementImage,
+            areaThumbnails,
+            thumbnailsElements
+        }) {
+            $("body").append(backgroundDark);
+            backgroundDark.append(area);
+            area.append(currentElementImage);
+            area.append(areaThumbnails);
+            thumbnailsElements.forEach(item => {
+                areaThumbnails.append(item);
+                console.log(item);
+            });
+        }
+
+        mountLightBox({
+            backgroundDark,
+            area,
+            currentElementImage,
+            areaThumbnails,
+            thumbnailsElements
+        });
+
+
+    }
+};
+
+const lightbox = new LightBoxProduct({
+    thumbnails: imagesProducts
+});
+
+imageCurrentProduct.click(() => {
+    lightbox.show();
+});
